073b2a2445efb0acc056b81aa55299bc40f89d6d
Alan <alanrjes@gmail.com>
2021-05-02 19:18:33 -0700

    tsp compiles
    Segmentation fault when run, but it compiles!

--- tsp.cc
+++ tsp.cc
@@ -8,11 +8,12 @@
 #include <fstream>
 #include <numeric>
 
-Cities::permutation_t random_search(Cities& cities, unsigned iters) {  // my own code from part 1
+// Randomized search, my own code from part 1
+Cities::permutation_t random_search(Cities& cities, unsigned iters) {
   Cities::permutation_t bestroute;
   double bestdist;
   for (int i=0; i<iters; i++) {
-    Cities::permutation_t nroute = cities.random_permutation();
+    Cities::permutation_t nroute = cities.random_permutation();  // generate random permutation
     double ndist = cities.total_path_distance(nroute);
     if (ndist < bestdist || !i) {  // update bestroute if better ordering of route or first iteration
       bestroute = nroute;
@@ -23,33 +24,28 @@ Cities::permutation_t random_search(Cities& cities, unsigned iters) {  // my own
   return bestroute;
 }
 
-// ga_search uses a genetic algorithm to solve the traveling salesperson problem for a given list of cities.
-// This function then creates a randomly generated population of permutations for traveling to those cities.
-// The function also requires a population size and mutation rate, to indicate how aggressively the population's individuals mutate.
-// The function then repeatedly evolves the population to generate increasingly better (i.e. shorter total distances) city permutations.
-// The best cities permutation is returned.
-Cities::permutation_t ga_search(Cities& cities, unsigned iters, unsigned pop_size, double mutation_rate) {
-  auto best_dist = 1e100;
-  auto best_ordering = Cities::permutation_t(cities.size());
+// Gentic algorithm search!
+// Create a random population of permutations (Deme), with a given population size and mutation rate, then repeatedly evolves for higher fitness (ie lower distance).
+Cities::permutation_t ga_search(Cities& cities, unsigned iters, unsigned population, double mutation) {
+  Deme deme(&cities, population, mutation);
+  Cities::permutation_t bestroute;
+  double bestdist;
 
-  Deme deme(&cities, pop_size, mutation_rate);
-
-  // Evolve the population to make it fitter and keep track of
-  // the shortest distance generated
-  for (long i = 1; i <= iters/pop_size; ++i) {
-    deme.compute_next_generation();    // generate next generation
-
-    // Find best individual in this population
-    const auto ordering = deme.get_best()->get_ordering();
-    if (is_improved(cities, ordering, best_dist, i * pop_size)) {
-      best_ordering = ordering;
+  for (int i=0; i<(iters/population); i++) {
+    deme.compute_next_generation();  // generate new generation
+    Cities::permutation_t nroute = deme.get_best() -> get_ordering();  // get best out of the generation
+    double ndist = cities.total_path_distance(nroute);
+    if (ndist < bestdist || !i) {  // update bestroute if better ordering of route or first iteration
+      bestroute = nroute;
+      bestdist = ndist;
+      std::cout << i << " " << bestdist << std::endl;
     }
   }
-  return best_ordering;
+  return bestroute;
 }
 
-
-int main(int argc, char* argv[]) {  // heavily reworked the version from moodle, to work with how I have Cities set up
+// heavily reworked from the version from moodle, to work with how I have Cities set up
+int main(int argc, char* argv[]) {
   if (argc != 4) {
     std::cerr << "Required arguments: filename for cities, population size, and mutation rate\n";
     return -1;
@@ -60,8 +56,8 @@ int main(int argc, char* argv[]) {  // heavily reworked the version from moodle,
   Cities cities;
   ifile >> cities;  // read file map to cities object
 
-  auto popsize = atoi(argv[2]);
-  auto mutrate = atof(argv[3]);
+  unsigned popsize = atoi(argv[2]);
+  double mutrate = atof(argv[3]);
 
   constexpr unsigned itercount = 100000;
 //  Cities::permutation_t bestroute = ga_search(cities, itercount, popsize, mutrate);
